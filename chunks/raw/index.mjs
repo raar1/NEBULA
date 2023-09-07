831c8525e585edec8bbda3cf76c7c316dc87c945
raar1 <raar1@users.noreply.github.com>
2023-09-07 13:39:41 +0000

    deploy: 2f6576c0576f61fea39ca0ce16a97cc490d2e00d

--- chunks/raw/index.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const index = "{\"parsed\":{\"_path\":\"/\",\"_dir\":\"\",\"_draft\":false,\"_partial\":false,\"_locale\":\"\",\"_empty\":false,\"title\":\"Hello world\",\"description\":\"Here is some markdown content.\",\"body\":{\"type\":\"root\",\"children\":[{\"type\":\"element\",\"tag\":\"h1\",\"props\":{\"id\":\"hello-world\"},\"children\":[{\"type\":\"text\",\"value\":\"Hello world\"}]},{\"type\":\"element\",\"tag\":\"p\",\"props\":{},\"children\":[{\"type\":\"text\",\"value\":\"Here is some markdown content.\"}]},{\"type\":\"element\",\"tag\":\"p\",\"props\":{},\"children\":[{\"type\":\"element\",\"tag\":\"a\",\"props\":{\"href\":\"/lessons/demo\"},\"children\":[{\"type\":\"text\",\"value\":\"Demo lesson link\"}]}]}],\"toc\":{\"title\":\"\",\"searchDepth\":2,\"depth\":2,\"links\":[]}},\"_type\":\"markdown\",\"_id\":\"content:index.md\",\"_source\":\"content\",\"_file\":\"index.md\",\"_extension\":\"md\"},\"hash\":\"SgE6uZ0NWP\"}";
-
-export { index as default };
-//# sourceMappingURL=index.mjs.map
